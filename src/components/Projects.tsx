ff87fd4f75c40f493d245b4fcacd073d1d7f0007
Amiya Ranjan Das <amiya.ranjan.das@sap.com>
2023-08-13 13:56:13 +0530

    github cards added

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,7 +6,8 @@ let Projects = () => {
       name: "",
       desc: "",
       language: "",
-      size: "",
+      size: "0",
+      starCount: "0",
     },
   ];
   const [data, setData] = useState(dataForm);
@@ -15,78 +16,46 @@ let Projects = () => {
     // fetch data
     const dataFetch = async () => {
       const gitData = await (
-        await fetch("https://api.github.com/users/GauravPrince-hub/repos")
+        await fetch("https://api.github.com/users/gaurav-prince/repos")
       ).json();
       const data = gitData
         .map((obj: any) => [
           {
             name: obj.name,
-            desc: obj.desc,
+            desc: obj.description,
             language: obj.language,
             size: obj.size,
+            starCount: obj.stargazers_count,
           },
         ])
         .flat();
       // set state when the data received
       setData(data);
-      // console.log(data);
+      console.log(data);
     };
     dataFetch();
   }, []);
 
+  const projectCards = data.map((repo) => {
+    if (parseInt(repo.starCount) > 0) {
+      return (
+        <div className="card col-md-3 project-card" key={repo.name}>
+          <div className="card-body">
+            <h5 className="card-title">{repo.name}</h5>
+            {/* <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6> */}
+            <p className="card-text">{repo.desc}</p>
+            <div className="card-link">{repo.language}</div>
+            <div className="card-link">{parseInt(repo.size) / 1000}</div>
+          </div>
+        </div>
+      );
+    }
+  });
+
   return (
     <div className="projects container">
       <h1 className="display-5 fw-bold">Open Source Projects!</h1>
-      <div className="row">
-        <div className="card col-md-3 offset-md-1 col-sm-5 project-card">
-          <div className="card-body">
-            <h5 className="card-title">Card Title</h5>
-            {/* <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6> */}
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="card-link">
-              Card link
-            </a>
-            <a href="#" className="card-link">
-              Another link
-            </a>
-          </div>
-        </div>
-        <div className="card col-md-3 project-card">
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            {/* <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6> */}
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="card-link">
-              Card link
-            </a>
-            <a href="#" className="card-link">
-              Another link
-            </a>
-          </div>
-        </div>
-        <div className="card col-md-3 project-card">
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            {/* <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6> */}
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="card-link">
-              Card link
-            </a>
-            <a href="#" className="card-link">
-              Another link
-            </a>
-          </div>
-        </div>
-      </div>
+      <div className="row">{projectCards}</div>
     </div>
   );
 };
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,11 +1,16 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
 let Skills = () => {
   return (
     <div className="skills">
       <h2>My Skills</h2>
-      <span id="skill-text">
+      <div id="skill-text">
         I thrive on embracing diverse technologies, constantly learning and
         implementing what I really like.
-      </span>
+      </div>
+      <FontAwesomeIcon icon={faCoffee} />
+      <FontAwesomeIcon icon="apple" />
     </div>
   );
 };
