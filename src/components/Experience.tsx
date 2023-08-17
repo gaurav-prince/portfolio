1e017846bc07e927abb47d70c9f3e675ded125ee
Gaurav Prince <gkumar475@gmail.com>
2023-08-18 01:43:47 +0530

    experience-card added

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -3,117 +3,68 @@ import companyImg2 from "../assets/ge_logo.svg";
 let Experience = () => {
   return (
     <div className="container experience" id="experience">
-      <h1>Experiences</h1>
+      <h1 id="experience-text">Experiences</h1>
       <div className="row">
-        <div className="col-md-6 experience-card-own">
-          <div className="row">
-            <div className="col-md-3 company-name" id="tcs-div">
-              <h4>Tata Consultancy Services</h4>
-            </div>
-            {/* <div className="col-md-1"> */}
-            <img
-              src={companyImg1}
-              alt="company1"
-              id="tcsimg"
-              className="col-md-1 company-icon"
-            />
-            {/* </div> */}
-            {/* <i className="col-md-1 bi bi-github company-icon"></i> */}
-            <div className="col-md-6">
-              <p>
-                Worked as a NodeJS Developer to build a cloud-based solution
-                hosted in Azure for a Utility-based client. The solution was to
-                upgrade an on-premise application to move to the cloud and add
-                multiple other features. The project was the world's
-                first-of-its-kind application. My key responsibilities in this
-                role were: • Develop new API endpoints in NodeJS • Deploy the
-                codebase in different environments using Azure DevOps • Host the
-                Application in AKS(Azure Kubernetes Service) within a Docker
-                Container. • Provide support in production after Go-live •
-                Mentor a team of 3 providing them with tasks
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-5 experience-card-own">
-          <div className="row">
-            <div className="col-md-3 company-name border-right" id="ge-div">
-              <h4 id="ge-text">General Electric Co.</h4>
-            </div>
-            <img
-              src={companyImg2}
-              alt="company2"
-              id="geimg"
-              className="col-md-1 company-icon"
-            />
-            {/* <i className="col-md-1 bi bi-github company-icon"></i> */}
-            <div className="col-md-6">
-              <p>
-                Currently, working as part of the Thread Connect Team which
-                provides Integrations solutions for other internal businesses.
-                The product is built on top of Apache NiFi. My key
-                responsibilities in the role are: • Develop new features within
-                the Application • Help with the Application upgrade process by
-                doing CVE Fixes • Deployment through Jenkins • Train new joiners
-                and provide them KT • Document the processes
-              </p>
-            </div>
-          </div>
-        </div>
-        {/* <div className="col-md-6">
-          <div className="experience-card">
-            <div
-              className="experience-banner"
-              // style="background: rgb(164, 36, 12);"
-            >
-              <div className="experience-blurred_div"></div>
-              <div className="experience-div-company">
-                <h5 className="experience-text-company">Quora</h5>
-              </div>
-              <img
-                className="experience-roundedimg"
-                src="/static/media/quoraLogo.85eb29f4d52320c8ed99.png"
-                alt="Quora"
-              />
-            </div>
-            <div className="experience-text-details">
-              <h5 className="experience-text-role">Front-End Developer</h5>
-
-              <h5 className="experience-text-date">May 2017 – May 2018</h5>
-              <p className="subTitle experience-text-desc">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-              <ul></ul>
-            </div>
-          </div>
-        </div> */}
-      </div>
-      {/* // ToDo */}
-      {/* <div className="row">
-        <div className="col-md-6 card">
-          <img src={companyImg1} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-          </div>
-        </div>
-        <div className="col-md-6 card">
+        <div className="col-md-5 card card-experience" id="ge-card">
           <img src={companyImg2} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+          <div className="card-body experience-card-body">
+            <h5 className="card-title experience-card-title">
+              General Electric Co.
+            </h5>
+            <h6 className="experience-card-subtitle mb-2">
+              Software Engineer (Jul-2022 - Present )
+            </h6>
+            <p className="experience-card-text">
+              Maintain and upgrade the in house product called Thread Connect,
+              developed on top of Apache NiFi. Along with it provide solutioning
+              and support to the customers.
             </p>
           </div>
+          <div className="row skill-used">
+            <ul>
+              <li className="skill-name badge rounded-pill">Java</li>
+              <li className="skill-name badge rounded-pill">Apache NiFi</li>
+              <li className="skill-name badge rounded-pill">Jenkins</li>
+              <li className="skill-name badge rounded-pill">Argo CD</li>
+              <li className="skill-name badge rounded-pill">SonarQube</li>
+              <li className="skill-name badge rounded-pill">Kubernetes</li>
+            </ul>
+          </div>
         </div>
-      </div> */}
+        <div
+          className="col-md-5 offset-md-1 card card-experience"
+          id="tcs-card"
+        >
+          <img src={companyImg1} className="card-img-top" alt="..." />
+          <div className="card-body experience-card-body">
+            <h5 className="card-title experience-card-title">
+              Tata Consultancy Services
+            </h5>
+            <h6 className="experience-card-subtitle mb-2">
+              IT Analyst (Jul 2017- Jul 2022)
+            </h6>
+            <p className="experience-card-text">
+              Developed the Reporting Module in NodeJS for a cloud-based
+              solution hosted in Azure for a Utility-based client who provide
+              energy services to more than 2 million people in the country.
+            </p>
+          </div>
+          <div className="row skill-used">
+            <ul>
+              <li className="skill-name badge rounded-pill">NodeJS</li>
+              <li className="skill-name badge rounded-pill">Express</li>
+              <li className="skill-name badge rounded-pill">TypeScript</li>
+              <li className="skill-name badge rounded-pill">Microsoft Azure</li>
+              <li className="skill-name badge rounded-pill">Azure Cosmos DB</li>
+              <li className="skill-name badge rounded-pill">
+                Azure Kubernetes Services
+              </li>
+              <li className="skill-name badge rounded-pill">RabbitMQ</li>
+              <li className="skill-name badge rounded-pill">CheckMarx</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 import "../App.css";
-// import headerLightImgUrl from "../assets/logo_3.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import headerLightImgUrl from "../assets/g-text.png";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import headerDarkImgUrl from "../assets/logo_light.png";
-import { faCode } from "@fortawesome/free-solid-svg-icons";
+// import { faCode } from "@fortawesome/free-solid-svg-icons";
 // import { slide as Menu, Props } from "react-burger-menu";
 import { useState } from "react";
 import DarkModeToggle from "react-dark-mode-toggle";
@@ -20,17 +20,18 @@ let Header = (props: any) => {
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            <FontAwesomeIcon icon={faCode} className="header-brand" />
+            {/* <FontAwesomeIcon icon={faCode} className="header-brand" />
             <h3 id="brand-name" className="header-brand">
               Gaurav Prince
-            </h3>
-            {/* <img
+            </h3> */}
+
+            <img
               src={headerLightImgUrl}
               alt="Gaurav Prince"
               id="headerImage"
-              //   width="30"
-              //   height="24"
-            /> */}
+              width="50"
+              height="50"
+            />
           </a>
           {/* <Menu>
             <a className="menu-link" href="#about">
@@ -49,11 +50,82 @@ let Header = (props: any) => {
               Contact Me
             </a>
           </Menu> */}
-          {/* <div className="hamburger-menu">
-            <button className="btn">
-              <FontAwesomeIcon icon={faBars} />
+          {/* <div className="container-fluid">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasNavbar"
+              aria-controls="offcanvasNavbar"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
             </button>
+            <div
+              className="offcanvas offcanvas-end"
+              id="offcanvasNavbar"
+              aria-labelledby="offcanvasNavbarLabel"
+            >
+              <div className="offcanvas-header">
+                <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                  Offcanvas
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="offcanvas"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="offcanvas-body">
+                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">
+                      Home
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Link
+                    </a>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <a
+                      className="nav-link dropdown-toggle"
+                      href="#"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      Dropdown
+                    </a>
+                    <ul className="dropdown-menu">
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          Action
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          Another action
+                        </a>
+                      </li>
+                      <li></li>
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          Something else here
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            
           </div> */}
+          {/* <button classNameName="btn">
+              <FontAwesomeIcon icon={faBars} />
+            </button> */}
           <div className="header-menu" id="navbarNavDropdown">
             <ul className="navbar-nav me-auto mb-2 ">
               <li className="nav-item">
